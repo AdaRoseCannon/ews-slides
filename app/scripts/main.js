eee908f12b9ea16b960722151ff23f251cdcf208
Ada Rose Edwards <ada@ada.is>
2015-04-16 22:24:20 +0100

    Slide styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
 const hogan = require('hogan');
 const utils = require('./lib/utils');
 
+function goToSlide(i) {
+	$('.slide').removeClass('active');
+	$(`#slide-${i}`).addClass('active');
+}
+
+window.goToSlide = goToSlide;
