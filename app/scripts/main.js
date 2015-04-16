aa74b625393b13ed5aaed08b398b6145f8ba4a16
Ada Rose Edwards <ada@ada.is>
2015-04-16 23:41:10 +0100

    Touch recognition

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,12 @@
 const hogan = require('hogan');
 const utils = require('./lib/utils');
 const slide = require('./lib/slides');
+const Hammer = require('hammerjs');
+
+var touches = new Hammer($('.slide-container')[0]);
+touches.set({ direction: Hammer.DIRECTION_HORIZONTAL });
+touches.on('swipeleft', goToNextSlide);
+touches.on('swiperight', goToPrevSlide);
 
 function goToSlide(i) {
 	const newSlide = $(`#slide-${i}`);
