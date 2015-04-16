06866331a06c4c1dbb503faf3a36d67a1dbb2c44
Ada Rose Edwards <ada@ada.is>
2015-04-16 23:05:33 +0100

    Setup and teardown functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,41 @@
 const hogan = require('hogan');
 const utils = require('./lib/utils');
+const slide = require('./lib/slides');
 
 function goToSlide(i) {
-	$('.slide').removeClass('active');
-	$(`#slide-${i}`).addClass('active');
+	const newSlide = $(`#slide-${i}`);
+	const oldSlide = $('.slide.active').attr('id');
+	if (newSlide[0]) {
+		$('.slide').removeClass('active');
+		$(`#slide-${i}`).addClass('active');
+		slide.setup(`slide-${i}`);
+		slide.teardown(oldSlide);
+	}
 }
 
+function goToNextSlide() {
+	goToSlide($('.slide.active').prevAll().length + 1);
+}
+
+function goToPrevSlide() {
+	goToSlide($('.slide.active').prevAll().length - 1);
+}
+
+window.addEventListener('keyup', e => {
+	switch(e.keyCode) {
+		case 37:
+			goToPrevSlide();
+			break;
+
+		case 13:
+		case 39:
+			goToNextSlide();
+			break;
+	}
+});
+
+goToSlide(0);
+
 window.goToSlide = goToSlide;
+window.goToNextSlide = goToNextSlide;
+window.goToPrevSlide = goToPrevSlide;
