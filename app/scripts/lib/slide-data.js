5d754b51aa7eb501b34007fc8a7d5b3cb36a6640
Ada Rose Edwards <ada@ada.is>
2015-04-19 19:34:51 +0100

    Amazing demo completed

--- app/scripts/lib/slide-data.js
+++ app/scripts/lib/slide-data.js
@@ -66,19 +66,193 @@ module.exports = {
 		setup() {},
 		action: function* () {
 
+			function displayBoundingRects(els) {
+				els.forEach(el => {
+					var dimensions = el.getBoundingClientRect();
+					var dimensionIndicator = $(`<div data-tl="${parseInt(dimensions.left, 10)}, ${parseInt(dimensions.top, 10)}" data-br="${parseInt(dimensions.right, 10)}, ${parseInt(dimensions.bottom, 10)}"></div>`).css(dimensions).addClass('dimensionIndicator');
+					$('.slide-container').append(dimensionIndicator);
+				});
+			}
+
+			var addNotification = function () {
+				let newNotification = $(templates.notification);
+				t.find('.notifications-go-here').prepend(newNotification);
+				return newNotification;
+			}.bind(this);
+
+			function unDeconstructedMagicTransform (fn, elementsToWatch, newTransition, callback) {
+				let measurements = elementsToWatch.map(el => {
+					let output = {
+						el,
+						initialDimensions: el.getBoundingClientRect(),
+						initialTransformToRestore: el.style.transform,
+						initialTransitionToRestore: el.style.transition
+					};
+					return output;
+				});
+
+				fn();
+
+				measurements.forEach(m => {
+					m.newDimensions = m.el.getBoundingClientRect();
+					m.el.style.transition = "0s";
+					let newScale = {
+						y: m.initialDimensions.height/m.newDimensions.height,
+						x: m.initialDimensions.width/m.newDimensions.width
+					};
+					let offset = {
+						x: m.initialDimensions.left - m.newDimensions.left,
+						y: m.initialDimensions.top - m.newDimensions.top
+					};
+					m.el.style.transformOrigin = "0 0 0";
+					m.el.style.transform = `translate(${offset.x * newScale.x}px, ${offset.y * newScale.y}px) scale(${newScale.x}, ${newScale.y}) ${m.el.style.transform}`;
+				});
+
+				measurements.forEach(m => {
+					let newScale = {
+						y: m.initialDimensions.height/m.newDimensions.height,
+						x: m.initialDimensions.width/m.newDimensions.width
+					};
+					$(m.el).children().toArray().forEach(el  => {
+						let elDimensions = el.getBoundingClientRect();
+						let offsetFromParent = {
+							x: elDimensions.left - m.initialDimensions.left,
+							y: elDimensions.top - m.initialDimensions.top
+						};
+						el.style.transformOrigin = `${offsetFromParent.x}px ${offsetFromParent.y}px`;
+						el.style.transform = `scale(${1/newScale.x}, ${1/newScale.y}) ${el.style.transform}`;
+					});
+				});
+
+				let transitionendFn = function () {
+					transitionendFn = function () {};
+					callback();
+				};
+
+				measurements.forEach(m => {
+					const transition = `transform ${newTransition}`;
+					m.el.style.transition = transition;
+					m.el.style.transform = m.initialTransformToRestore;
+					$(m.el).children().toArray().forEach(el  => {
+						el.style.transition = transition;
+						el.style.transform = "";
+					});
+					if (callback) {
+						m.el.addEventListener('transitionend', transitionendFn);
+					}
+				});
+			}
+
 			var t = $(this).find('.render-here');
 			var templates = Templates['slide-3'];
+			var newTransition = "3s ease";
 
 			t.html(templates.demoApp);
-			var addNotification = function () {
-				t.find('.notifications-go-here').prepend($(templates.notification));
-			}.bind(this);
 			setTimeout(addNotification, 500);
 			timeouts.s3 = setInterval(addNotification, 2000);
+			yield;
+
+			clearInterval(timeouts.s3);
+			t.html(templates.demoApp);
+			yield;
+
+			let tempNotification;
+			let elementsToWatch = t.find('.panel:not(.pretend-web-app)').toArray();
+			unDeconstructedMagicTransform(
+				() => {
+					tempNotification = addNotification();
+					tempNotification.css({
+						opacity: 0
+					});
+				},
+				elementsToWatch,
+				newTransition,
+				() => {
+					tempNotification.css({
+						transition: 'opacity 0.3s ease',
+						opacity: 1
+					});
+				}
+			);
+			yield;
+
+			t.html(templates.demoApp);
+			yield;
+
+			elementsToWatch = t.find('.panel:not(.pretend-web-app)').toArray();
+			let measurements = elementsToWatch.map(el => {
+				let output = {
+					el,
+					initialDimensions: el.getBoundingClientRect(),
+					initialTransformToRestore: el.style.transform,
+					initialTransitionToRestore: el.style.transition
+				};
+				return output;
+			});
+
+			displayBoundingRects(elementsToWatch);
+			yield;
+
+			$('.dimensionIndicator').remove();
+			addNotification();
+			yield;
+
+			measurements.forEach(m => {
+				m.newDimensions = m.el.getBoundingClientRect();
+				m.el.style.transition = "0s";
+			});
+
+			displayBoundingRects(elementsToWatch);
+			yield;
+
+			$('.dimensionIndicator').remove();
+			yield;
+
+			measurements.forEach(m => {
+				let newScale = {
+					y: m.initialDimensions.height/m.newDimensions.height,
+					x: m.initialDimensions.width/m.newDimensions.width
+				};
+				let offset = {
+					x: m.initialDimensions.left - m.newDimensions.left,
+					y: m.initialDimensions.top - m.newDimensions.top
+				};
+				m.el.style.transformOrigin = "0 0 0";
+				m.el.style.transform = `translate(${offset.x * newScale.x}px, ${offset.y * newScale.y}px) scale(${newScale.x}, ${newScale.y}) ${m.el.style.transform}`;
+			});
+			yield;
+
+			measurements.forEach(m => {
+				let newScale = {
+					y: m.initialDimensions.height/m.newDimensions.height,
+					x: m.initialDimensions.width/m.newDimensions.width
+				};
+				$(m.el).children().toArray().forEach(el  => {
+					let elDimensions = el.getBoundingClientRect();
+					let offsetFromParent = {
+						x: elDimensions.left - m.initialDimensions.left,
+						y: elDimensions.top - m.initialDimensions.top
+					};
+					el.style.transformOrigin = `${offsetFromParent.x}px ${offsetFromParent.y}px`;
+					el.style.transform = `scale(${1/newScale.x}, ${1/newScale.y}) ${el.style.transform}`;
+				});
+			});
+			yield;
+
+			measurements.forEach(m => {
+				const transition = `transform ${newTransition}`;
+				m.el.style.transition = transition;
+				m.el.style.transform = m.initialTransformToRestore;
+				$(m.el).children().toArray().forEach(el  => {
+					el.style.transition = transition;
+					el.style.transform = "";
+				});
+			});
 			yield;
 		},
 		teardown() {
 			clearInterval(timeouts.s3);
+			$('.dimensionIndicator').remove();
 			$(this).find('.render-here').html('');
 		}
 	},
