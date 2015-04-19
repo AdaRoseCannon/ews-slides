b7b093b15ecd25131223f8b0bce31c4c9a86058c
Ada Rose Edwards <ada@ada.is>
2015-04-19 01:24:07 +0100

    2nd draft

---
+++ app/scripts/lib/slide-data.js
@@ -0,0 +1,98 @@
+var marked = require('marked');
+var Templates = require('./templates');
+var timeouts = {};
+
+module.exports = {
+	'slide-0': {
+		setup() {},
+		action: function* () {
+			yield;
+		},
+		teardown() {}
+	},
+	'slide-1': {
+		setup() {},
+		action: function* () {
+
+			var t = $(this).find('.render-here');
+			var templates = Templates['slide-1'];
+			yield;
+
+			t.html(templates[0]);
+			yield;
+
+			t.html(marked(templates[1]));
+			yield;
+
+			t.html(marked(templates[2]));
+			yield;
+		},
+		teardown() {
+			$(this).find('.render-here').html('');
+		}
+	},
+	'slide-2': {
+		setup() {},
+		action: function* () {
+
+			var t = $(this).find('.render-here');
+			var templates = Templates['slide-2'];
+
+			t.html('');
+			var i = 0;
+			var pre = $('<pre></pre>');
+			t.append(pre);
+			timeouts.s2 = setInterval(() => {
+				pre.append(i++ % 2 === 1 ? 'myVar = el.getBoundingClientRect().height;' : 'el.height = (myVar + 1) + "px"');
+				pre.append('\n');
+			}, 800);
+			yield;
+
+			clearInterval(timeouts.s2);
+			t.html(templates[0]);
+			yield;
+
+			t.html(marked(templates[1]));
+			yield;
+
+		},
+		teardown() {
+			clearInterval(timeouts.s2);
+			$(this).find('.render-here').html('');
+		}
+	},
+	'slide-3': {
+		setup() {},
+		action: function* () {
+
+			var t = $(this).find('.render-here');
+			var templates = Templates['slide-3'];
+
+			t.html(templates.demoApp);
+			timeouts.s3 = setInterval(() => {
+				console.log('boo');
+				$(this).find('.notifications-go-here').prepend($(templates.notification));
+			}, 3000);
+			yield;
+		},
+		teardown() {
+			clearInterval(timeouts.s3);
+			$(this).find('.render-here').html('');
+		}
+	},
+	'slide-4': {
+		setup() {},
+		action: function* () {
+
+			var t = $(this).find('.render-here');
+			var templates = Templates['slide-4'];
+			yield;
+			t.html(marked(templates.containment));
+			yield;
+		},
+		teardown() {
+			clearInterval(timeouts.s3);
+			$(this).find('.render-here').html('');
+		}
+	},
+};
--- app/scripts/lib/slides.js
+++ app/scripts/lib/slides.js
@@ -1,88 +1,27 @@
-var marked = require('marked');
-var Templates = require('./templates');
-var timeout;
-
-var slideFuncs = {
-	'slide-0': {
-		setup() {},
-		action: function* () {
-			yield;
-		},
-		teardown() {}
-	},
-	'slide-1': {
-		setup() {		},
-		action: function* () {
-
-			var t = $(this).find('.render-here');
-			var templates = Templates['slide-1'];
-			yield;
-
-			t.html(templates[0]);
-			yield;
-
-			t.html(marked(templates[1]));
-			yield;
-
-			t.html(marked(templates[2]));
-			yield;
-		},
-		teardown() {
-			$(this).find('.render-here').html('');
-		}
-	},
-	'slide-2': {
-		setup() {		},
-		action: function* () {
-
-			var t = $(this).find('.render-here');
-			var templates = Templates['slide-2'];
-			yield;
-
-			t.html(templates[0]);
-			yield;
-
-
-			t.html('');
-			var i = 0;
-			var pre = $('<pre></pre>');
-			t.append(pre);
-			timeout = setInterval(() => {
-				pre.append(i++ % 2 === 1 ? 'myVar = el.getBoundingClientRect().height;' : 'el.height = (myVar + 1) + "px"');
-				pre.append('\n');
-			}, 800);
-			yield;
-
-		},
-		teardown() {
-			clearInterval(timeout);
-			$(this).find('.render-here').html('');
-		}
-	},
-};
+const slideData = require('./slide-data');
 
 module.exports.triggerEvent = {
 	next() {}
 };
 
 module.exports.setup = function (name) {
-	if (slideFuncs[name]) {
-		slideFuncs[name].setup.bind(document.getElementById(name))();
+	if (slideData[name]) {
+		slideData[name].setup.bind(document.getElementById(name))();
 	} else {
-		slideFuncs[name] = {
+		slideData[name] = {
 			setup() {},
 			action: function* (){yield;},
 			teardown() {}
 		};
 	}
-	module.exports.triggerEvent = slideFuncs[name].action.bind(document.getElementById(name))();
+	module.exports.triggerEvent = slideData[name].action.bind(document.getElementById(name))();
 
 	// Do first action
 	module.exports.triggerEvent.next();
 };
 
 module.exports.teardown = function (name) {
-	if (slideFuncs[name]) {
-		slideFuncs[name].teardown.bind(document.getElementById(name))();
+	if (slideData[name]) {
+		slideData[name].teardown.bind(document.getElementById(name))();
 	}
 };
